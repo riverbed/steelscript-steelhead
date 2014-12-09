5ee3a6039c3798d8e3ec49c00ee4ac8a71a635ca
lsouza <lsouza@riverbed.com>
2014-12-09 09:32:13 -0800

    Adding action for 'common' REST service

---
+++ steelscript/steelhead/services/common/v8_5/action.py
@@ -0,0 +1,37 @@
+# Copyright (c) 2014 Riverbed Technology, Inc.
+#
+# This software is licensed under the terms and conditions of the MIT License
+# accompanying the software ("License").  This software is distributed "AS IS"
+# as set forth in the License.
+
+from __future__ import (absolute_import, unicode_literals, print_function,
+                        division)
+
+from steelscript.common.interaction import action, uidelegation
+
+
+@action.action
+class CommonAction(action.Action):
+    """
+    Kauai Actions for the 'common' REST Service on the SteelHead product.
+    """
+
+
+class CLI(uidelegation.CLIDelegatee):
+    """
+    CLI-based Actions for the 'common' REST Service on the SteelHead product.
+    """
+
+    def get_product_info(self):
+        """
+        Gets basic software and hardware product information.
+
+        :return: Dictionary of values:
+            {'name': 'SteelHead',
+             'model': 'CX1555',
+             'release': '9.0.1'}
+        """
+        version_info = self.model.show_version()
+        return {'name': 'SteelHead',
+                'model': version_info['product model'],
+                'release': version_info['product release']}
